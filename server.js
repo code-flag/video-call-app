9455621b6e67b0e985668f0bf48b903e9b223e88
code-flag <awefrancolaz@gmail.com>
2023-08-27 19:12:26 +0100

    chore: io server updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const server = require("http").createServer(app);
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
@@ -22,7 +22,6 @@ app.use(express.static("public"));
 app.locals.classTitle = "JSS3/Mathematics wk2";
 
 app.get("/", (req, res) => {
-  console.log("jhgsuy", req.headers.host);
   res.redirect(`/${uuidv4()}`);
 });
 
