08b52938e1a1530fb8a0381cbe7b01792dec054a
code-flag <awefrancolaz@gmail.com>
2023-08-27 18:22:25 +0100

    chore: script host change to render

--- public/script.js
+++ public/script.js
@@ -39,7 +39,8 @@ showChat.addEventListener("click", () => {
 const user = prompt("Enter your name");
 
 var peer = new Peer({
-  host: '127.0.0.1',
+  // host: '127.0.0.1',
+  host: "https://jvs-video-app.onrender.com",
   port: "3030",
   path: '/peerjs',
   config: {
