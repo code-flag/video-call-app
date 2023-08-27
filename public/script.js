0419836d5bbc61e1e9f6491d7d14dae1cf446e43
code-flag <awefrancolaz@gmail.com>
2023-08-27 19:32:42 +0100

    chore: pear option updatedf

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ var screenSharing = false;
 
 
 
-const socket = io("/");
+const socket = io("https://jvs-video-app.onrender.com/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
@@ -41,31 +41,32 @@ const user = prompt("Enter your name");
 var peer = new Peer({
   // host: '127.0.0.1',
   host: "https://jvs-video-app.onrender.com",
-  port: '',
-  path: '/peerjs',
-  config: {
-    'iceServers': [
-      { url: 'stun:stun01.sipphone.com' },
-      { url: 'stun:stun.ekiga.net' },
-      { url: 'stun:stunserver.org' },
-      { url: 'stun:stun.softjoys.com' },
-      { url: 'stun:stun.voiparound.com' },
-      { url: 'stun:stun.voipbuster.com' },
-      { url: 'stun:stun.voipstunt.com' },
-      { url: 'stun:stun.voxgratia.org' },
-      { url: 'stun:stun.xten.com' },
-      {
-        url: 'turn:192.158.29.39:3478?transport=udp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      },
-      {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      }
-    ]
-  },
+  secure: true,
+  // port: '',
+  // path: '/peerjs',
+  // config: {
+  //   'iceServers': [
+  //     { url: 'stun:stun01.sipphone.com' },
+  //     { url: 'stun:stun.ekiga.net' },
+  //     { url: 'stun:stunserver.org' },
+  //     { url: 'stun:stun.softjoys.com' },
+  //     { url: 'stun:stun.voiparound.com' },
+  //     { url: 'stun:stun.voipbuster.com' },
+  //     { url: 'stun:stun.voipstunt.com' },
+  //     { url: 'stun:stun.voxgratia.org' },
+  //     { url: 'stun:stun.xten.com' },
+  //     {
+  //       url: 'turn:192.158.29.39:3478?transport=udp',
+  //       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+  //       username: '28224511:1379330808'
+  //     },
+  //     {
+  //       url: 'turn:192.158.29.39:3478?transport=tcp',
+  //       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+  //       username: '28224511:1379330808'
+  //     }
+  //   ]
+  // },
 
   debug: 3
 });
