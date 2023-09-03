4122b8f519321fabab66f4997d481af7ba87c055
code-flag <awefrancolaz@gmail.com>
2023-09-03 22:16:31 +0100

    chore: iceserver added

--- public/script.js
+++ public/script.js
@@ -42,31 +42,37 @@ var peer = new Peer({
   secure: true,
   // port: '',
   path: '/peerjs',
+  port: 10000,
+  debug: 3,
+  
   config: {
     'iceServers': [
-      { url: 'stun:stun01.sipphone.com' },
-      { url: 'stun:stun.ekiga.net' },
-      { url: 'stun:stunserver.org' },
-      { url: 'stun:stun.softjoys.com' },
-      { url: 'stun:stun.voiparound.com' },
-      { url: 'stun:stun.voipbuster.com' },
-      { url: 'stun:stun.voipstunt.com' },
-      { url: 'stun:stun.voxgratia.org' },
-      { url: 'stun:stun.xten.com' },
       {
-        url: 'turn:192.158.29.39:3478?transport=udp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
+        urls: "stun:stun.relay.metered.ca:80",
       },
       {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      }
-    ]
+        urls: "turn:a.relay.metered.ca:80",
+        username: "eeb64afd49d5799fe41de5ac",
+        credential: "IbwBDjAqpxDBfF1Y",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:80?transport=tcp",
+        username: "eeb64afd49d5799fe41de5ac",
+        credential: "IbwBDjAqpxDBfF1Y",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443",
+        username: "eeb64afd49d5799fe41de5ac",
+        credential: "IbwBDjAqpxDBfF1Y",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443?transport=tcp",
+        username: "eeb64afd49d5799fe41de5ac",
+        credential: "IbwBDjAqpxDBfF1Y",
+      },
+  ]
   },
   port: 10000,
-
   debug: 3
 });
 
